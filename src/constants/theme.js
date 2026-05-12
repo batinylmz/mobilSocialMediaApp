7ebfc6dd851d978351c8ade3844e099109695a31
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-12 23:59:04 +0300

    -theme.js dosyasi olusturldu. -theme.js dosyasi içerisine renk kodlari ve olusturuldu.

---
+++ src/constants/theme.js
@@ -0,0 +1,34 @@
+// src/constants/theme.js
+
+export const COLORS = {
+    background: '#FFFFFF',
+    textMain: '#000000',
+    textSecondary: '#6B7280',
+    tagBackground: '#E5E7EB',
+    border: '#D1D5DB',
+    notificationBadge: '#EF4444',
+
+    // Yeni Gradient Renkleri
+    gradientBlue: '#2A7AE2',
+    gradientBlackPaylas: 'rgba(0, 0, 0, 0.8)', // %80 Opaklık
+    gradientBlackGiris: '#312727',
+    gradientRed: '#DB6565',
+};
+
+export const SIZES = {
+    // ... (Önceki ölçüler aynen kalsın) ...
+    titleInputWidth: 400,
+    titleInputHeight: 40,
+    contentInputWidth: 400,
+    contentInputHeight: 200,
+    inputRadius: 10,
+    logoWidth: 39.87,
+    logoHeight: 37,
+    notificationSize: 30,
+    notificationRight: 11,
+    mediaWidth: 396,
+    mediaHeight: 220,
+    mediaRadius: 10,
+    bottomTabWidth: 390,
+    bottomTabHeight: 30,
+};
