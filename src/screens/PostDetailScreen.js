109968ce38d18da7977776f498ad065ad79226ea
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-15 02:39:59 +0300

    -Alt gezinme cubugundaki person ikonu eklendi.

--- src/screens/PostDetailScreen.js
+++ src/screens/PostDetailScreen.js
@@ -148,6 +148,9 @@ const PostDetailScreen = () => {
                 <TouchableOpacity style={styles.navIconContainer}>
                     <Icon name="add-circle" size={30} color="#000000" />
                 </TouchableOpacity>
+                <TouchableOpacity style={styles.navIconContainer}>
+                    <Icon name="person" size={26} color="#000000" />
+                </TouchableOpacity>
                 </View>
 
 </SafeAreaView>
