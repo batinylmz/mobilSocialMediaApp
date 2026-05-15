59c69bd3f84dac56a167b7c17494f2f8622c9de5
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-15 14:17:24 +0300

    -Add-circle ikonu buttomnavbar'a eklendi.

--- src/components/BottomNavBar.js
+++ src/components/BottomNavBar.js
@@ -12,6 +12,9 @@ const BottomNavBar = () => {
             <TouchableOpacity style={styles.navIconContainer}>
                 <Icon name="compass" size={26} color="#000000" />
             </TouchableOpacity>
+            <TouchableOpacity style={styles.navIconContainer}>
+                <Icon name="add-circle" size={30} color="#000000" />
+            </TouchableOpacity>
         </View>
     );
 };
