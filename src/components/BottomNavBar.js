acd599700cd9cc16d1d8ad4204b0bec8b9be597b
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-15 14:16:35 +0300

    -Home ikonu buttomnavbar'a eklendi.

--- src/components/BottomNavBar.js
+++ src/components/BottomNavBar.js
@@ -2,3 +2,13 @@ import React from 'react';
 import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { COLORS } from '../constants/theme';
+
+const BottomNavBar = () => {
+    return (
+        <View style={styles.bottomNav}>
+            <TouchableOpacity style={styles.navIconContainer}>
+                <Icon name="home" size={26} color="#000000"/>
+            </TouchableOpacity>
+        </View>
+    );
+};
