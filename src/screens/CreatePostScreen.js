3aa89094c3a5df6a8901eb5bad714cf9722987b8
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-15 14:34:44 +0300

    -CreatePost ekraninda kaydirma işlemi için scrollview import edildi.

--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, SafeAreaView, Image } from 'react-native';
+import { View, Text, TextInput, StyleSheet,ScrollView, TouchableOpacity, SafeAreaView, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'; // Çan ikonu için eklendi
 import { COLORS, SIZES } from '../constants/theme';
 import BottomNavBar from '../components/BottomNavBar';
