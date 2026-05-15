9ca4119983f96a6b9df15593cbe689a6b26f5676
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-16 02:04:10 +0300

    -App.js importlar düzenlendi create ekranida burda importu yapildi.

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'react-native';
 import { PostProvider } from './src/context/PostContext';
-import PostDetailScreen from './src/screens/PostDetailScreen'; // Yeni ekranı import ettik
+import PostDetailScreen from './src/screens/PostDetailScreen';
+import CreatePostScreen from './src/screens/CreatePostScreen';// Yeni ekranı import ettik
 import { COLORS } from './src/constants/theme';
 const App = () => {
     return (
