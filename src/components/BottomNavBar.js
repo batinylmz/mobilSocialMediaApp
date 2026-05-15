08ae14a2012ce3afdcef2ba4c0393bdef0590d99
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-15 14:15:08 +0300

    -ButtomNavBar.js dosaysi olsuturuldu ekranin en altinda bulunan alt gezinme cubugu için ve importlar tamamlandi.

---
+++ src/components/BottomNavBar.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { COLORS } from '../constants/theme';
