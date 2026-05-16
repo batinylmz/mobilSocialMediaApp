528a240c0b98e9eef7bae42dd8f773e86a2eba6d
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-16 04:05:32 +0300

    -BottomNavBar içinde navigasyon kutuphanesini import ettik kutuphaneyi tanımladık ve buttona basım ıcın onpress eklendi.

--- src/components/BottomNavBar.js
+++ src/components/BottomNavBar.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native'; // 1. Navigasyon kütüphanesini import ettik
 import { COLORS } from '../constants/theme';
 
 const BottomNavBar = () => {
+    const navigation = useNavigation(); // 2. Kütüphaneyi burada tanımladık
+
     return (
         <View style={styles.bottomNav}>
-            <TouchableOpacity style={styles.navIconContainer}>
+            <TouchableOpacity style={styles.navIconContainer} onPress={() => navigation.navigate('PostDetail')}>
                 <Icon name="home" size={26} color="#000000"/>
             </TouchableOpacity>
+
             <TouchableOpacity style={styles.navIconContainer}>
                 <Icon name="compass" size={26} color="#000000" />
             </TouchableOpacity>
-            <TouchableOpacity style={styles.navIconContainer}>
+
+            {/* Ortadaki + Butonu artık CreatePost ekranını açacak */}
+            <TouchableOpacity style={styles.navIconContainer} onPress={() => navigation.navigate('CreatePost')}>
                 <Icon name="add-circle" size={30} color="#000000" />
             </TouchableOpacity>
+
             <TouchableOpacity style={styles.navIconContainer}>
                 <Icon name="person" size={26} color="#000000" />
             </TouchableOpacity>
+
             <TouchableOpacity style={styles.navIconContainer}>
                 <Icon name="settings" size={26} color="#000000" />
             </TouchableOpacity>
@@ -39,10 +47,11 @@ const styles = StyleSheet.create({
         paddingBottom: 5,
     },
     navIconContainer: {
-        width: 30,
-        height: 30,
+        width: 36,
+        height: 36,
         justifyContent: 'center',
         alignItems: 'center',
-    },
+    }
 });
+
 export default BottomNavBar;
