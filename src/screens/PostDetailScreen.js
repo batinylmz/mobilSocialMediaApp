8e71ee9d5beaa55434c86dd2099fa565529144bf
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-17 16:06:56 +0300

    -PostDetailScreen.js dosyasinda useState ve useRef import edildi.

--- src/screens/PostDetailScreen.js
+++ src/screens/PostDetailScreen.js
@@ -1,4 +1,4 @@
-import React ,{ useContext } from 'react';
+import React ,{ useContext,useState, useRef } from 'react';
 import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity, Image, ScrollView,TextInput } from 'react-native';
 import { PostContext } from '../context/PostContext';
 import Icon from 'react-native-vector-icons/Ionicons';
