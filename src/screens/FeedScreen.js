9b1c6991ce82c78875181325ed1a9178258f18c0
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-17 17:52:33 +0300

    -FeedScreen.js dosyasi içerisinde importlar tamamlandi.

---
+++ src/screens/FeedScreen.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { SafeAreaView, View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+// import { COLORS } from '../constants/theme'; // Kendi renk dosyan varsa kullanabilirsin
