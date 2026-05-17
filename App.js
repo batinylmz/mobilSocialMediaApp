975d13a27475a01516804428cc46ed75449ac547
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-17 19:57:29 +0300

    -App.js güncellendi.

--- App.js
+++ App.js
@@ -3,11 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'react-native';
 
-// Ekran İçe Aktarımları (Importlar)
-import FeedScreen from './src/screens/FeedScreen'; // YENİ: Ana Akış ekranımızı import ettik
+import FeedScreen from './src/screens/FeedScreen';
 import PostDetailScreen from './src/screens/PostDetailScreen';
 import CreatePostScreen from './src/screens/CreatePostScreen';
-import { COLORS } from './src/constants/theme';
 import { PostProvider } from './src/context/PostContext';
 
 const Stack = createNativeStackNavigator();
@@ -17,16 +15,11 @@ const App = () => {
         <PostProvider>
             <NavigationContainer>
                 <Stack.Navigator
-                    initialRouteName="Feed" // YENİ: Uygulama artık ilk olarak Feed (Ana Akış) ekranıyla açılacak
-                    screenOptions={{ headerShown: false }} // Kendi özel header'larımızı kullandığımız için bunu gizliyoruz
+                    initialRouteName="Feed"
+                    screenOptions={{ headerShown: false }}
                 >
-                    {/* 1. Ana Akış Ekranı */}
                     <Stack.Screen name="Feed" component={FeedScreen} />
-
-                    {/* 2. Gönderi Detay Ekranı */}
                     <Stack.Screen name="PostDetail" component={PostDetailScreen} />
-
-                    {/* 3. Gönderi Oluşturma Ekranı */}
                     <Stack.Screen name="CreatePost" component={CreatePostScreen} />
                 </Stack.Navigator>
             </NavigationContainer>
