432c3ffc14ec295eefd59deead753fa791b92e11
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-17 18:08:06 +0300

    -FeedScreen.js dosyasi içerisinde ScrollView import edildi.

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet, Image, TouchableOpacity,TextInput } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, Image, TouchableOpacity,TextInput,ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 // import { COLORS } from '../constants/theme'; // Kendi renk dosyan varsa kullanabilirsin
 
